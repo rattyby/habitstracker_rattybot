f86dd7e57fe7dd13841566f00006442ac11f8f7e
Петр Кравцов <rat.f.br@gmail.com>
2026-05-06 17:09:51 +0300

    re-fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,15 +13,15 @@ from models import Base
 TEST_DATABASE_URL = 'sqlite+aiosqlite:///:memory:'
 
 
-# @pytest.fixture(autouse=True)
-# def set_env():
-#     """Устанавливает все переменные окружения, нужные для импорта бота и БД"""
-#     os.environ['BOT_TOKEN'] = '1234567890:AAHdqTcvCH1vGWJxfSeofSAs0K5PALDsaw'
-#     os.environ['DB_HOST'] = 'localhost'
-#     os.environ['DB_PORT'] = '5432'
-#     os.environ['DB_USER'] = 'test_user'
-#     os.environ['DB_PASSWORD'] = 'test_pass'
-#     os.environ['DB_NAME'] = 'test_db'
+@pytest.fixture(autouse=True)
+def set_env():
+    """Устанавливает все переменные окружения, нужные для импорта бота и БД"""
+    os.environ['BOT_TOKEN'] = '1234567890:AAHdqTcvCH1vGWJxfSeofSAs0K5PALDsaw'
+    os.environ['DB_HOST'] = 'localhost'
+    os.environ['DB_PORT'] = '5432'
+    os.environ['DB_USER'] = 'test_user'
+    os.environ['DB_PASSWORD'] = 'test_pass'
+    os.environ['DB_NAME'] = 'test_db'
 
 
 @pytest.fixture(scope='session')
